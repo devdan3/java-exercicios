ddfa887ac346451a55b9a47e97234593ed43da13
Daniel Tavares <danieltavaressouza13@gmail.com>
2026-05-18 14:19:26 -0300

    Melhorando solução com POO

--- desafio vetores_pensionato/src/Main.java
+++ desafio vetores_pensionato/src/Main.java
@@ -1,34 +1,33 @@
+import entites.Rent;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        System.out.print("How many rooms will be rented? ");
+        Scanner sc = new Scanner(System.in);
+        Rent[] vect = new Rent[10];
+
+        System.out.print("How many rooms will be rented: ");
         int n = sc.nextInt();
-        String[] name = new String[10];
-        String[] email = new String[10];
-        int roomNumber;
-        String nametemp, emailtemp;
 
         for(int i = 0; i < n; i++){
+            System.out.println();
             System.out.println("Rent #" + (i + 1));
-            sc.nextLine();
             System.out.print("Name: ");
-            nametemp = sc.nextLine();
+            sc.nextLine();
+            String name = sc.nextLine();
             System.out.print("Email: ");
-            emailtemp = sc.nextLine();
+            String email = sc.next();
             System.out.print("Room: ");
-            roomNumber = sc.nextInt();
-            name[roomNumber] = nametemp;
-            email[roomNumber] = emailtemp;
+            int numberRoom = sc.nextInt();
+            vect[numberRoom] = new Rent(name, email);
         }
-        System.out.println("Busy rooms");
-        for(int i = 0; i < name.length; i++){
-            if(name[i] != null){
-                System.out.println(i + ": " + name[i] + ", " + email[i]);
+        System.out.println();
+        System.out.println("Busy rooms:");
+        for(int i = 0; i < vect.length; i++)
+            if(vect[i] != null)
+                System.out.println(i + ": " + vect[i].toString());
 
-            }
-        }
+        sc.close();
     }
 }
