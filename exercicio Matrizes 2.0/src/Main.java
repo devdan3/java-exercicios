11cdd8602adbc8dc4e3418b7d6963ed1e6a5e6e3
Daniel Tavares <danieltavaressouza13@gmail.com>
2026-05-27 16:50:16 -0300

    Exercício sobre Matrizes

---
+++ exercicio Matrizes 2.0/src/Main.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o número de linhas da sua matriz: ");
+        int linhas = sc.nextInt();
+        System.out.print("Digite o número de colunas da sua matriz: ");
+        int colunas = sc.nextInt();
+
+        int[][] matriz = new int[linhas][colunas];
+
+        for(int i = 0; i < matriz.length; i++) {
+            for(int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Digite um número pertencente a matriz e veja os números q estão a sua volta:");
+        System.out.print("N: ");
+        int ocorrencia = sc.nextInt();
+
+        for(int i = 0; i < matriz.length; i++) {
+            for(int j = 0; j < matriz[i].length; j++) {
+                if(matriz[i][j] == ocorrencia){
+                    System.out.println();
+                    System.out.println("Position: " + i + "," + j );
+                    if(j > 0) {
+                        System.out.println("Left: " + matriz[i][j - 1]);
+                    }
+                    if(i > 0) {
+                        System.out.println("Up: " + matriz[i - 1][j]);
+                    }
+                    if(j < matriz[i].length - 1) {
+                        System.out.println("Right: " + matriz[i][j + 1]);
+                    }
+                    if(i < matriz.length - 1){
+                        System.out.println("Down: " + matriz[i + 1][j]);
+                    }
+                }
+            }
+        }
+        sc.close();
+    }
+}
