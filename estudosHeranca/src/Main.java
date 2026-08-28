d88aafc709ac3a4a339724349adcc2e14304726a
Daniel Tavares <danieltavaressouza13@gmail.com>
2026-08-28 13:16:39 -0300

    Adicionando sobreposição, palavra super e anotação @Override

--- estudosHeranca/src/Main.java
+++ estudosHeranca/src/Main.java
@@ -31,5 +31,17 @@ public class Main {
             acc5.updateBalance();
             System.out.println("Update!");
         }
+
+        Account acc6 = new Account(1005, "Daniel", 1000.0);
+        acc6.withdraw(200.0);
+        System.out.println(acc6.getBalance());
+
+        Account acc7 = new SavingsAccount(1006, "Marcelo", 1000.0, 0.01);
+        acc7.withdraw(200.0);
+        System.out.println(acc7.getBalance());
+
+        Account acc8 = new BusinessAccount(1007, "Leo", 1000.0, 500.0);
+        acc8.withdraw(200.0);
+        System.out.println(acc8.getBalance());
     }
 }
