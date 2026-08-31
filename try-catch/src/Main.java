3e2692015bdce1f4a32a8e027745e7c4fa071512
Daniel Tavares <danieltavaressouza13@gmail.com>
2026-08-31 14:01:24 -0300

    Estudando a estrutura try-catch

---
+++ try-catch/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            String[] vect = sc.nextLine().split(" ");
+            int position = sc.nextInt();
+            System.out.println(vect[position]);
+        }
+        catch (ArrayIndexOutOfBoundsException exception) {
+            System.out.println("Invalid Position!");
+        }
+        catch (InputMismatchException exception) {
+            System.out.println("Input error!");
+        }
+
+        System.out.println("End of Program");
+
+    }
+}
